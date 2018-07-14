dcc071716df5af92aaa26b8aad88ad8b60f5966c
glavigna <gonzalolavigna@gmail.com>
2018-07-14 19:01:45 -0300

    Se prueba la funcion de bloque de char y anda bien.

--- practicas/practica_01_04/src/test.c
+++ practicas/practica_01_04/src/test.c
@@ -4,16 +4,19 @@
 
 void testUpdate (void * taskParmPtr){
 	uint8_t uart_usb_byte_recibido;
+	gpioInit(GPIO2,GPIO_OUTPUT);
 	while(1){
+		gpioWrite(GPIO2,ON);
 		if(uartReadByte(UART_USB,&uart_usb_byte_recibido)){
 			if (sw_uart_sent(uart_usb_byte_recibido) == TRUE){
 				uartWriteByte(UART_USB,uart_usb_byte_recibido);
 			}
 			else {
-				printf("\nBYTE RECHAZADO:");
-				uartWriteByte(UART_USB,uart_usb_byte_recibido);
-				printf("\n");
+				printf("\nBYTE RECHAZADO:%c\n",uart_usb_byte_recibido);
+				//uartWriteByte(UART_USB,uart_usb_byte_recibido);
+
 			}
 		}
+		gpioWrite(GPIO2,OFF);
 	}
 }
