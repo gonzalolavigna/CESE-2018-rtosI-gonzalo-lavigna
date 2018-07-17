c3de09a678671f55e7f898ed111fbb447c97e0fd
glavigna <gonzalolavigna@gmail.com>
2018-07-17 20:56:27 -0300

    Se comenta el ejemplo de 7 segmentos en el project.mk

--- project.mk
+++ project.mk
@@ -13,8 +13,8 @@
 #PROJECT_PATH = examples/c/sapi/bare_metal
 #PROJECT_NAME = sapi_basic_example
 
-PROJECT_PATH = examples/c/sapi/bare_metal
-PROJECT_NAME = keypad_7segment_01
+#PROJECT_PATH = examples/c/sapi/bare_metal
+#PROJECT_NAME = keypad_7segment_01
 
 # ---------- Example of path outside this file folder ------------------
 #PROJECT_PATH = ../examples/c/sapi/bare_metal
